6e2d3b5c675523d5515bbce088258604e94a7fd1
Ritesh Malviya <luckyriteshmalviya@gmail.com>
2025-07-15 20:50:43 +0530

    added fees

--- src/pages/processes/Payment.tsx
+++ src/pages/processes/Payment.tsx
@@ -523,7 +523,7 @@ export default function Payment({
                         type="number"
                         value={additionalAmount}
                         onChange={(e) => setAdditionalAmount(e.target.value)}
-                        placeholder="200"
+                        placeholder="00"
                         min="0"
                         step="0.01"
                         className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
