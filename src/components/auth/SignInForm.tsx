4d42b5e13a6503b1913938f369261b820d18e5e0
Ritesh Malviya <luckyriteshmalviya@gmail.com>
2025-07-24 13:24:52 +0530

    removed theme toggle button

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Label from "../form/Label";
 import Input from "../form/input/InputField";
 import Button from "../ui/button/Button";
