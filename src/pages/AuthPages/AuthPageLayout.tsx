ff6831b971a5bbab386bb7d235ea41df5848f4c7
Ritesh Malviya <luckyriteshmalviya@gmail.com>
2025-08-07 13:43:09 +0530

    updated the background of signin page

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -12,14 +12,14 @@ export default function AuthLayout({
     <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
       <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
         {children}
-        <div className="items-center hidden w-full h-full lg:w-1/2 bg-gradient-to-br from-brand-400 to-brand-950 dark:from-gray-800 dark:to-gray-900 lg:grid">
+        <div className="items-center hidden w-full h-full lg:w-1/2 bg-gradient-to-br to-brand-950 dark:from-gray-800 dark:to-gray-900 lg:grid">
           <div className="relative flex items-center justify-center z-1">
             {/* <!-- ===== Common Grid Shape Start ===== --> */}
             <GridShape />
             <div className="flex flex-col items-center">
               <Link to="/" className="block">
                 <img
-                  src="/images/logo/accountouch logo-b.png"
+                  src="/images/logo/accountouch logo-a.png"
                   alt="AccountTouch Logo"
                   className="w-72 h-auto drop-shadow-[0_4px_12px_rgba(0,0,0,0.3)] hover:drop-shadow-[0_6px_16px_rgba(0,0,0,0.4)] transition-shadow duration-300"
                   onError={(e) => {
